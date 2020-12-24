3557e57f70415f5b87b5399d9c9ebd424e7dd11b
zayson <jymaeng95@gmail.com>
2020-12-24 20:02:17 +0900

    final, abstract practice

---
+++ src/AbstractCar.java
@@ -0,0 +1,35 @@
+public abstract class AbstractCar {
+    abstract public void run();
+    abstract public void stop();
+    public void accel() {
+        System.out.println("액셀");
+    }
+
+    class Tucson extends  AbstractCar{
+
+        @Override
+        public void run() {
+            System.out.println("투싼이 달린다");
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("투싼이 멈춘다.");
+        }
+    }
+
+    class Sportage extends AbstractCar{
+
+        @Override
+        public void run() {
+            System.out.println("스포티지가 달린다");
+        }
+
+        @Override
+        public void stop() {
+            System.out.println("스포티지가 멈춘다");
+        }
+    }
+
+}
+
---
+++ src/FinalClass.java
@@ -0,0 +1,19 @@
+public  class FinalClass {
+    final double PI = 3.141592;
+    int x = 10;
+    int y = 20;
+    int sum() {
+        return this.x + this.y;
+    }
+
+    final int mul() {
+        return this.x - this.y;
+    }
+
+    class Test extends FinalClass {
+        @Override
+        int sum() {
+            return super.sum();
+        }
+    }
+}
