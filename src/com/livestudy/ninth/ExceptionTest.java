dc6cb449df9c47994025ca85a09c47acfd7f0e73
zayson <jymaeng95@gmail.com>
2021-01-12 10:44:35 +0900

    individual study

--- src/com/livestudy/ninth/ExceptionTest.java
+++ src/com/livestudy/ninth/ExceptionTest.java
@@ -5,13 +5,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ExceptionTest {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int[] test = {1};
         try {
             br.close();
+            System.out.println(test[1]);
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("IO Exception");
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("array exception");
         }
+
+        try {
+            System.out.println(test[1]);
+        }catch (Exception  e){
+
+        }
+
+        try{
+            System.out.println(sumPositiveNum(-1));
+        } catch (Exception e ){
+            e.printStackTrace();
+        }
+    }
+
+    // N까지의 양수의 합
+    private static int sumPositiveNum(int N) throws Exception {
+        if( N < 0)
+            throw new Exception("N은 양의 정수이어야 합니다");
+
+        int sum = 0;
+        for(int i =1;i<=N;i++){
+            sum += i;
+        }
+        return sum;
     }
 }
