1bf2e443e109caf69414b0a600d1d5e42065c9a4
zayson <jymaeng95@gmail.com>
2021-01-15 23:59:08 +0900

    individual study

--- src/com/livestudy/ninth/CustomExceptionApp.java
+++ src/com/livestudy/ninth/CustomExceptionApp.java
@@ -3,17 +3,22 @@ package com.livestudy.ninth;
 public class CustomExceptionApp {
     public static void main(String[] args) {
         try{
-            getAge(2022);
+            checkAge(2022);
         } catch (CustomException e){
             e.printStackTrace();
+            System.out.println("ERR_MSG : "+ e.getMessage());
             System.out.println("ERR_CODE : "+ e.getErrorCode());
         }
     }
 
-    private static void getAge(int birthYear) throws CustomException {
+    private static void checkAge(int birthYear) throws CustomException {
         final int YEAR = 2021;
+
+        //탄생년도가 2021년 보다 큰 경우
         if(YEAR - birthYear < 0)
             throw new CustomException("Too Big Birth Year");
+
+        //탄생년도가 1800년 보다 작은 경우
         if(birthYear < 1800)
             throw new CustomException("Maybe Wrong Birth Year",100);
     }
