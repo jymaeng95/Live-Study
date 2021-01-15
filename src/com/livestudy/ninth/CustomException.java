4cf54c885f4029a46d7d1c3a0cc93b4e9141d1b5
zayson <jymaeng95@gmail.com>
2021-01-15 23:53:05 +0900

    individual study

--- src/com/livestudy/ninth/CustomException.java
+++ src/com/livestudy/ninth/CustomException.java
@@ -3,15 +3,18 @@ package com.livestudy.ninth;
 public class CustomException extends Exception {
     private final int ERR_CODE;
 
+    //에러 메세지(커스텀)
     public CustomException(String message) {
         this(message,200);
     }
 
+    // 에러 메세지(커스텀), 에러 코드(커스텀)
     public CustomException(String message, int ERR_CODE) {
         super(message);
         this.ERR_CODE = ERR_CODE;
     }
 
+    // 에러코드 리턴
     public int getErrorCode() {
         return ERR_CODE;
     }
