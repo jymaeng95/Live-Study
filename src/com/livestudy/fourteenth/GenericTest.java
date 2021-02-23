2dc4e555c2c0b1f83d5568dbfd7f18f42a9fcc0d
zayson <jymaeng95@gmail.com>
2021-02-24 00:12:07 +0900

    individual study

--- src/com/livestudy/fourteenth/GenericTest.java
+++ src/com/livestudy/fourteenth/GenericTest.java
@@ -12,6 +12,10 @@ public class GenericTest {
         System.out.println("이름 : "+intPerson.getName());
         System.out.println("성별 : "+intPerson.getGender());
 
+        // Number의 하위타입만 가능하도록 제한
 //        People<String> people = new People<String>();
+
+        //Number의 하위타입인 Integer는 가능
+        People<Integer> people = new People<>();
     }
 }
